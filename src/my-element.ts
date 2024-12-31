81819dec63f6a5827bd411fdcf6959a2d84c15c5
James Bampoe <james.bampoe@mgb.ch>
2024-12-31 11:36:38 +0100

    feat: learn about reactive properties
    https://lit.dev/docs/components/properties/

--- src/my-element.ts
+++ src/my-element.ts
@@ -6,6 +6,7 @@
 
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import {dateConverter} from './custom-converters/date-converter';
 
 /**
  * An example element.
@@ -23,12 +24,20 @@ export class MyElement extends LitElement {
       padding: 16px;
       max-width: 800px;
     }
+
+    :host([active]) {
+      border: 1px solid red;
+    }
   `;
 
   /**
    * The name to say "Hello" to.
    */
-  @property()
+  @property({
+    hasChanged(newValue: string, oldValue: string) {
+      return newValue?.toLowerCase() !== oldValue?.toLowerCase();
+    },
+  })
   name = 'World';
 
   /**
@@ -37,12 +46,64 @@ export class MyElement extends LitElement {
   @property({type: Number})
   count = 0;
 
+  // observed attribute name is "myvalue"
+  // Use the default converter
+  @property({type: Number})
+  myValue = 0;
+
+  // Observed attribute will be called my-name
+  @property({attribute: 'my-name'})
+  myName = 'Ogden';
+
+  /**
+   * No observed attribute for this property.
+   * The property will not be initialized from attributes in markup, and attribute changes won't affect it.
+   */
+  @property({attribute: false})
+  myData = {};
+
+  /**
+   * Property with a custom converter
+   * Value of property "date" will reflect to attribute "date".
+   * Reflecting properties to attributes should be done sparingly
+   *
+   * Reflecting properties of type object or array is not recommended.
+   * This can cause large objects to serialize to the DOM which can result in poor performance.
+   */
+  @property({converter: dateConverter(navigator.language), reflect: true})
+  date = new Date();
+
+  /**
+   * Boolean properties must default to false
+   */
+  @property({type: Boolean, reflect: true})
+  active = false;
+
   override render() {
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+
     return html`
       <h1>${this.sayHello(this.name)}!</h1>
       <button @click=${this._onClick} part="button">
         Click Count: ${this.count}
       </button>
+      <p>myValue = ${this.myValue}</p>
+      <p>myName = ${this.myName}</p>
+      <p>
+        date = ${this.date.toLocaleDateString(navigator.language, dateOptions)}
+      </p>
+
+      <section>
+        <p>active = ${this.active}</p>
+        <button @click="${() => (this.active = !this.active)}">
+          Toggle active
+        </button>
+      </section>
       <slot></slot>
     `;
   }
